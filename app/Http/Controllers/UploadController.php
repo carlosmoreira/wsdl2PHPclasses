4e1f6c78e6a3213555e3a99ce4ccbd1b65cc0c8f
Carlos Moreira <moreira.carlos09@gmail.com>
2016-09-17 00:30:38 -0400

    Refactored the create of classes and zip files into a facade

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -2,53 +2,24 @@
 
 namespace App\Http\Controllers;
 
-use App\library\GeneratorConfigFactory;
-use Comodojo\Zip\Zip;
-use Illuminate\Auth\Access\Response;
+use App\library\WsdlToClass;
 use Illuminate\Http\Request;
-
 use App\Http\Requests;
 use Mockery\CountValidator\Exception;
-use Wsdl2PhpGenerator\Generator;
-
 
 class UploadController extends Controller
 {
     public function uploadWsdl(Request $request)
     {
-        try{
+        try {
             if (!$request->hasFile('wsdlfile'))
                 throw new Exception('WSDL File is required');
 
-            //pass in wsdl
-
-            //$wsdlToClass = new WsdlToClass($request->all());
-
-
-            //return zip file
-
-            $tmpDir = uniqid();
-
-            $generator = new Generator();
-            $generator->generate(GeneratorConfigFactory::generateConfig($request->all(), $tmpDir));
-
-            $zip = Zip::create($tmpDir . '.zip');
-            $zip->add($tmpDir);
-            $zipName = $zip->getZipFile();
-            $zip->close();
-
-            $zipContent = file_get_contents($_SERVER['DOCUMENT_ROOT'] . '\\' . $zip->getZipFile());
-
-            if (PHP_OS === 'WINNT'){
-                exec("rd /s /q $tmpDir");
-                exec("del $zipName");
-            }else {
-                exec("rm -rf $tmpDir");
-                exec("rm -rgf $zipName");
-            }
+            $wsdlToClass = new WsdlToClass($request->all());
+            $zipContent = $wsdlToClass->getZip();
 
             return response($zipContent)->header('Content-Type', 'application/gzip')->header('Content-Disposition', 'attachment; filename=phpclasses.zip');
-        }catch (Exception $e){
+        } catch (Exception $e) {
             return redirect('/')->withInput()->with('error', $e->getMessage());
         }
 
--- app/library/WsdlToClass.php
+++ app/library/WsdlToClass.php
@@ -6,14 +6,31 @@
  * Time: 11:40 PM
  */
 
-namespace app\library;
+namespace App\library;
 
 
+use Comodojo\Zip\Zip;
 use Illuminate\Http\UploadedFile;
+use Wsdl2PhpGenerator\Generator;
 
+/**
+ * Class WsdlToClass
+ * @package App\library
+ * Description: Facade class used to generate and return the zip file with all classes
+ */
 class WsdlToClass
 {
 
+    /**
+     * @var array
+     */
+    private $requestData;
+
+    /**
+     * @var string
+     */
+    private $tmpDir;
+    
     /**
      * @var $wsdlFile UploadedFile
      */
@@ -23,22 +40,44 @@ class WsdlToClass
      * WsdlToClass constructor.
      * @param UploadedFile $wsdlFile
      */
-    public function __construct(UploadedFile $wsdlFile)
+    public function __construct(array $data)
     {
-        $this->wsdlFile = $wsdlFile;
+        if(!isset($data['wsdlfile']))
+            throwException('wsdl file not found in wsdlToClass');
+        $this->requestData = $data;
+        $this->wsdlFile = $data['wsdlfile'];
+        $this->tmpDir = uniqid();
     }
 
+    /**
+     * @return mixed
+     * @throws \Comodojo\Exception\ZipException
+     */
     public function getZip(){
-        $tmpDir = uniqid();
-
+        //Generate files
         $generator = new Generator();
-        $generator->generate(GeneratorConfigFactory::generateConfig($request->all(), $tmpDir));
+        $generator->generate(GeneratorConfigFactory::generateConfig($this->requestData, $this->tmpDir));
 
-        $zip = Zip::create($tmpDir . '.zip');
-        $zip->add($tmpDir);
+        //Create Zip and add files
+        $zip = Zip::create($this->tmpDir . '.zip');
+        $zip->add($this->tmpDir);
+
         $zipName = $zip->getZipFile();
         $zip->close();
 
+        $fileContent = file_get_contents($_SERVER['DOCUMENT_ROOT'] . '\\' . $zip->getZipFile());
+
+        //Delete files
+        if (PHP_OS === 'WINNT'){
+            exec("rd /s /q $this->tmpDir");
+            exec("del $zipName");
+        }else {
+            exec("rm -rf $this->tmpDir");
+            exec("rm -rgf $zipName");
+        }
+
+        //Return zip as content
+        return $fileContent;
     }
 
 
